a1edcc82f35e7598909f02f13833852174f92542
EthanSund <esund19@outlook.com>
2024-12-12 07:16:57 +0000

    flatpickr initialised and configured

--- frontend/src/components/Calendar/CalendarPage.js
+++ frontend/src/components/Calendar/CalendarPage.js
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./CalendarPage.css";
 import { useUser } from "../../context/UserContext";
+import flatpickr from "flatpickr";
+
+flatpickr(element, {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        mode: "multiple",
+        minDate: "today",
+        maxDate: new Date().fp_incr(14)
+});
+
+
+
+
 
 export default CalendarPage;
