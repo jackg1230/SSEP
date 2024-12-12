0a90b912c3a56a114d06fcb95517f7245bd1a10b
jackg1230 <jackgough67@gmail.com>
2024-12-12 13:31:15 +0000

    Update CalendarPage.js

--- frontend/src/components/Calendar/CalendarPage.js
+++ frontend/src/components/Calendar/CalendarPage.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from "react";
-
-import { useUser } from "../../context/UserContext";
+import React, { useEffect } from "react";
 import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"; // Import Flatpickr styles
 
+function CalendarPage() {
+    useEffect(() => {
+        flatpickr("#datePicker", {
+            mode: "multiple",
+            altInput: true,
+            altFormat: "F j, Y",
+            dateFormat: "Y-m-d",
+            minDate: "today",
+            onChange: function (selectedDates, dateStr, instance) {
+                // If more than 3 dates are selected, remove the most recently added date
+                if (selectedDates.length > 3) {
+                    selectedDates.pop();
+                    instance.setDate(selectedDates, false); // Update the Flatpickr instance
+                    alert("Maximum of 3 dates allowed!");
+                }
+            },
+        });
+    }, []); // Empty dependency array ensures this runs only once
 
-
-
-function CalendarPage(){
-
-    flatpickr("#datePicker", {
-        mode: "multiple",
-        altInput: true,
-        altFormat: "F j, Y",
-        dateFormat: "Y-m-d",
-        minDate: "today",
-        onChange: function(selectedDates, dateStr, instance) {
-            // If more than 3 dates are selected, remove the most recently added date
-            if (selectedDates.length > 3) {
-                selectedDates.pop();
-                instance.setDate(selectedDates, false); // Update the Flatpickr instance
-                alert("Maximum of 3 dates allowed!");
-            }
-        }
-    });
-
-    return(
+    return (
         <div className="CalendarPage">
-            <input type="text" id="datePicker" placeholder="Select dates"></input>
+            <form>
+                <label htmlFor="datePicker">Choose dates:</label>
+                <input type="text" id="datePicker" name="date-picker" />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
+
 export default CalendarPage;
